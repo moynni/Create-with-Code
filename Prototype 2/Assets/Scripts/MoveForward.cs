ce9e1751b587b53d7bfc626bbf1bb6336556a3e2
SkygenStudios <49883367+SkygenStudios@users.noreply.github.com>
2019-09-29 22:23:48 -0400

    prot. 2 update .2.1
    +Character Movement
    +Pizza moves

---
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26a747de86fcd48b9662b09d2e3741f63562279680e82f20b85fda1071689111
+size 413
---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3125eae2e567b7145ddcc9925f6a60c2f252e88c60971c678198f577b5bfa412
+size 903
