8ff8c0f659a31f9214886d98e61a57735c85ccab
SkygenStudios <49883367+SkygenStudios@users.noreply.github.com>
2019-09-29 23:05:20 -0400

    Prot. 2 0.2.2
    Added in prefabs for pizza and animals
    
    Made it so animal's and pizza's are destroyed once it goes a certein distance in the z axis (-) or (+)
    
    Added in the abilty to shoot pizzas using space bar
    
    My plan is to have this game useable on a mobile search engine, but this may change as development continues.

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b32f740331098d740db73d0e5b30725a04924752780dd3e7d612b9de989e4e2f
+size 588
--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3125eae2e567b7145ddcc9925f6a60c2f252e88c60971c678198f577b5bfa412
-size 903
+oid sha256:7225ea142c68eb7da90d72e475629ff266204d8b131cf4ea9c389f70711b2fe5
+size 1115
